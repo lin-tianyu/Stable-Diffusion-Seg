9c7293a2e58e668bcfbcb6ff552e0bc79eb783d7
lin-tianyu <linty6@mail2.sysu.edu.cn>
2024-03-13 12:07:14 +0800

    update BTCV STS-3d preprocess code

--- data/sts3d/sts3d_preprocess.py
+++ data/sts3d/sts3d_preprocess.py
@@ -14,7 +14,6 @@ import torch
 from skimage import measure, morphology
 
 
-
 def show_slice(x, y=None):
     if y is not None:
         plt.subplot(121)
@@ -59,19 +58,16 @@ def process_pipe(x):
 
 if __name__ == "__main__":
 
-    root_path = "/GPUFS/nsccgz_ywang_zfd/LinTianyu/AIM/UniMatch/more-scenarios/medical/dataset"
+    root_path = "."
     save_path = os.path.join(root_path, "data")
     root_labelled = os.path.join(root_path, "labelled")
-    # root_unlabelled = os.path.join(root_path, "unlabelled")   # NO NEED
-    # root_test = os.path.join(root_path, "test")               # NO NEED
     stop_idx = 10e10
     labeled_slice, all_slice = 0, 0
 
-    """ for labeled"""
-    with open("labeledall.txt", "w+") as f:
+    """ for all labeled: train+test -> slices """
+    with open(os.path.join(root_path, "labeledall.txt"), "w+") as f:
         train_path = glob.glob(os.path.join(root_labelled, "image", "**"))
-        # assert len(train_path)==9
-        for idx, path in tqdm(enumerate(train_path), desc="processing unlabeled data"):
+        for idx, path in tqdm(enumerate(train_path), desc="processing labeled data"):
             img = nib.load(path).get_fdata()
             gts = nib.load(path.replace("image", "label")).get_fdata()
             img_mask = (img != img.min())
@@ -113,106 +109,39 @@ if __name__ == "__main__":
                 sys.exit(0)
         f.close()
 
-    """ for test"""
-    # with open("test.txt", "w+") as f:
-    #     val_path = glob.glob(os.path.join(root_test, "image", "**"))
-    #     print(val_path)
-    #     for idx, path in tqdm(enumerate(val_path), desc="processing unlabeled data"):
-    #         # print(path)
-    #         img = nib.load(path).get_fdata()
-    #         # gts = nib.load(path.replace("image", "label")).get_fdata()
-    #         # print(img.shape, gts.shape)
-    #         z = img.shape[-1]
-    #         assert img.shape[0] == img.shape[1]
-    #         assert len(img.shape) == 3
-    #         # assert img.shape == gts.shape
-    #
-    #         img = torch.tensor(img).permute(2, 0, 1).numpy()
-    #         # gts = torch.tensor(gts).permute(2, 0, 1).numpy()
-    #         # print(img.shape, gts.shape)
-    #
-    #         img_processed = process_pipe(img)
-    #         # print(minmax(img_processed), minmax(gts))
-    #
-    #         save_path_h5 = os.path.join(save_path, path.split("/")[-1].split(".")[0] + ".h5")
-    #         f.write(
-    #             os.path.join("data", path.split("/")[-1].split(".")[0] + ".h5") + "\n"
-    #         )
-    #         with h5py.File(save_path_h5, mode="w") as hf:
-    #             hf.create_dataset(name="image", dtype=float, data=img_processed)
-    #             hf.create_dataset(name="label", dtype=float, data=None)
-    #         hf.close()
-    #
-    #         if idx == stop_idx:
-    #             sys.exit(0)
-    #     f.close()
+    """ for valtest: test -> volumes """
+    with open("valtest.txt", "w+") as f:
+        val_path = glob.glob(os.path.join(root_labelled, "image", "*_001*_*"))
+        print(val_path)
+        for idx, path in tqdm(enumerate(val_path), desc="processing unlabeled data"):
+            # print(path)
+            img = nib.load(path).get_fdata()
+            gts = nib.load(path.replace("image", "label")).get_fdata()
+            # print(img.shape, gts.shape)
+            z = img.shape[-1]
+            assert img.shape[0] == img.shape[1]
+            assert len(img.shape) == 3
+            assert img.shape == gts.shape
+    
+            img = torch.tensor(img).permute(2, 0, 1).numpy()
+            gts = torch.tensor(gts).permute(2, 0, 1).numpy()
+            # print(img.shape, gts.shape)
+    
+            img_processed = process_pipe(img)
+            # print(minmax(img_processed), minmax(gts))
+    
+            save_path_h5 = os.path.join(save_path, path.split("/")[-1].split(".")[0] + ".h5")
+            f.write(
+                os.path.join("data", path.split("/")[-1].split(".")[0] + ".h5") + "\n"
+            )
+            # print(save_path_h5)
+            with h5py.File(save_path_h5, mode="w") as hf:
+                hf.create_dataset(name="image", dtype=float, data=img_processed)
+                hf.create_dataset(name="label", dtype=float, data=gts)
+            hf.close()
+    
+            if idx == stop_idx:
+                sys.exit(0)
+        f.close()
 
-    """ for valtest """
-    # with open("valtest.txt", "w+") as f:
-    #     val_path = glob.glob(os.path.join(root_labelled, "image", "*_001*_*"))
-    #     print(val_path)
-    #     for idx, path in tqdm(enumerate(val_path), desc="processing unlabeled data"):
-    #         # print(path)
-    #         img = nib.load(path).get_fdata()
-    #         gts = nib.load(path.replace("image", "label")).get_fdata()
-    #         # print(img.shape, gts.shape)
-    #         z = img.shape[-1]
-    #         assert img.shape[0] == img.shape[1]
-    #         assert len(img.shape) == 3
-    #         assert img.shape == gts.shape
-    #
-    #         img = torch.tensor(img).permute(2, 0, 1).numpy()
-    #         gts = torch.tensor(gts).permute(2, 0, 1).numpy()
-    #         # print(img.shape, gts.shape)
-    #
-    #         img_processed = process_pipe(img)
-    #         # print(minmax(img_processed), minmax(gts))
-    #
-    #         save_path_h5 = os.path.join(save_path, path.split("/")[-1].split(".")[0] + ".h5")
-    #         f.write(
-    #             os.path.join("data", path.split("/")[-1].split(".")[0] + ".h5") + "\n"
-    #         )
-    #         # print(save_path_h5)
-    #         with h5py.File(save_path_h5, mode="w") as hf:
-    #             hf.create_dataset(name="image", dtype=float, data=img_processed)
-    #             hf.create_dataset(name="label", dtype=float, data=gts)
-    #         hf.close()
-    #
-    #         if idx == stop_idx:
-    #             sys.exit(0)
-    #     f.close()
 
-    """ for unlabeled"""
-    # with open("unlabeled.txt", "w+") as f:
-    #     for idx, path in tqdm(
-    #             enumerate(glob.glob(os.path.join(root_unlabelled, "*.nii.gz")), ),
-    #             total=len(glob.glob(os.path.join(root_unlabelled, "*.nii.gz"))),
-    #             desc="processing unlabeled data"
-    #     ):
-    #         img = nib.load(path).get_fdata()
-    #         z = img.shape[-1]
-    #         assert img.shape[0] == img.shape[1]
-    #         assert len(img.shape) == 3
-    #
-    #         img = process_pipe(img)
-    #
-    #         for slice_idx in tqdm(range(z), desc=f"single volume"):
-    #             img_slice = img[:, :, slice_idx]
-    #             if img_slice.min() == img_slice.max():  # skip empty slice
-    #                 continue
-    #
-    #             save_path_h5 = os.path.join(save_path, ".".join(path.split("/")[-1].split(".")[:-2]) + f"_slice_{slice_idx}.h5")
-    #             f.write(
-    #                 os.path.join("data", ".".join(path.split("/")[-1].split(".")[:-2]) + f"_slice_{slice_idx}.h5")+"\n"
-    #             )
-    #             with h5py.File(save_path_h5, mode="w") as hf:
-    #                 hf.create_dataset(name="image", dtype=float, data=img_slice)
-    #                 hf.create_dataset(name="label", dtype=float, data=None)
-    #             hf.close()
-    #
-    #             if slice_idx == stop_idx:
-    #                 sys.exit(0)
-    #
-    #         if idx == stop_idx:
-    #             sys.exit(0)
-    #     f.close()
--- data/synapse/nii2format.py
+++ data/synapse/nii2format.py
@@ -1,5 +1,5 @@
 """
-Transforming Synapse Dataset into .npz format for Dataloader.
+Transforming BTCV Dataset into .npz format for Dataloader.
 Also, generating .txt files that contain .npz file name.
 - Train set .npz: 'image' data and 'label' data pair
 - Test  set .npz: 'image' data
@@ -15,13 +15,9 @@ import cv2
 from pprint import pprint
 from PIL import Image
 
-# from VID import VisualizeImageDistribution
 
-# pd.options.display.max_rows = None
 
-__all__ = ['Synapse', 'Abdomen', 'Cervix']
-
-test_vol_num = [
+TEST_VOLUME = [
     "0008",
     "0022",
     "0038",
@@ -34,27 +30,16 @@ test_vol_num = [
     "0004",
     "0025",
     "0035",
-]  # 12 testing volume, following `TransUnet`, `TFCNs`, etc.
+]  # 12 testing volume, following `TransUnet`, etc.
 
 
 
 def generate_vol_txt():
-    # for set in tqdm(__all__, total=3, desc='generating "test_vol.txt"s'):
     dataset = "Abdomen"
-    txt = os.path.join(root_path, 'lists', dataset, 'test_vol.txt')
-    imgpath = sorted(glob.glob(os.path.join(root_path, 'dataset', dataset, 'Rawdata/Training/img', '*.nii.gz')))
-    # labelpath = sorted(glob.glob(os.path.join(root_path, 'dataset', set, 'Rawdata/Training/label', '*.nii.gz')))
-    #
-    # # setting qfac to 1 to avoid warning
-    # for path_img, path_gts in tqdm(zip(imgpath, labelpath),
-    #                                desc="Setting qfac to 1! "):
-    #     image, label = nib.load(path_img), nib.load(path_gts)
-    #     image.header['pixdim'][0], label.header['pixdim'][0] = 1, 1
-    #     image.to_filename(path_img)
-    #     label.to_filename(path_gts)
+    imgpath = sorted(glob.glob(os.path.join(root_path, dataset, 'Rawdata/Training/img', '*.nii.gz')))
 
     test_imgpath = list(filter(lambda x:
-                               x if "".join(list(filter(str.isdigit, x.split('/')[-1].split('.')[0]))) in test_vol_num
+                               x if "".join(list(filter(str.isdigit, x.split('/')[-1].split('.')[0]))) in TEST_VOLUME
                                else False, imgpath))
 
     return test_imgpath
@@ -120,15 +105,18 @@ def generate_npz(img_path, test_vol_path, mode="all"):
                 for cls in range(14):
                     pixels_count[cls] += (slice_gts[:, :, 0] == cls).sum()
 
-                # saving
+                # save paths
                 save_img = os.path.join(
                     save_path[train_set],
                     imgpath.split('/')[-1].split('.')[0].replace('img', 'vol') + f"_{slice}.npy")
                 save_gts = os.path.join(
                     save_path[train_set],
                     imgpath.split('/')[-1].split('.')[0].replace('img', 'vol') + f"_{slice}.png")
-                # np.save(save_img, slice_img)
-                # Image.fromarray(slice_gts).save(save_gts)
+                
+                # saving
+                np.save(save_img, slice_img)
+                Image.fromarray(slice_gts).save(save_gts)
+
         print(min_edge, "minimum edge for %s :" % dataset, min(min_edge))
         print(f"[slices for {train_set} set]: {slice_counter}")
 
@@ -136,54 +124,29 @@ def generate_npz(img_path, test_vol_path, mode="all"):
         print(pixels_count / pixels_count.sum())
 
     operate_data(train_image_path, train_label_path, train_set=True)
-    # Train set pixel distribution:
-    # [9.54271542e-01 5.38177007e-03 2.30164099e-03 2.31975002e-03
-    #  4.22884468e-04 2.51331787e-04 2.44881894e-02 5.99445428e-03
-    #  1.59142062e-03 1.18954373e-03 5.27832639e-04 1.12456102e-03
-    #  5.87094159e-05 7.63698597e-05]
     operate_data(test_image_path, test_label_path, train_set=False)
-    # Test set pixel distribution (DON'T USE IN TRAINING):
-    # [9.57442868e-01 3.37351585e-03 2.13884821e-03 2.15849584e-03
-    # 2.88223734e-04 1.79689758e-04 2.37474150e-02 6.42523474e-03
-    # 1.07622147e-03 1.36420192e-03 4.45735698e-04 1.22695066e-03
-    # 6.09573053e-05 7.16423502e-05]
-
 
 if __name__ == '__main__':
-    """
-    README: modify 'dataset' for selection
-    """
     dataset = 'Abdomen'
-    assert dataset in __all__
 
-    # VID = VisualizeImageDistribution()
     info = pd.DataFrame(columns=['max', 'min', 'mean', 'std', 'var', 'shape', 'dtype', 'type', 'grad', 'is_cuda',
                                  'set', 'datatype'],
                         index=None)
 
-    root_path = "/GPUFS/nsccgz_ywang_zfd/LinTianyu/Internship/TFCNs/"
-    save_root = "/GPUFS/nsccgz_ywang_zfd/LinTianyu/Internship/stable-diffusion/data/synapse"
+    root_path = "."
+    save_root = "."
 
     # ../dataset: .nii.gz files
     # test_path = os.path.join(root_path, 'dataset', dataset, 'Rawdata', 'Testing', 'img')
     train_path = dict(
-        image=os.path.join(root_path, 'dataset', dataset, 'Rawdata', 'Training', 'img'),
-        label=os.path.join(root_path, 'dataset', dataset, 'Rawdata', 'Training', 'label')
+        image=os.path.join(root_path, dataset, 'Rawdata', 'Training', 'img'),
+        label=os.path.join(root_path, dataset, 'Rawdata', 'Training', 'label')
     )
 
     # ../data: .npz files
     save_path = dict(
         train=os.path.join(save_root, 'train'),
         test=os.path.join(save_root, 'test')
-    )
-    # save_path = dict(
-    #     train="/GPUFS/nsccgz_ywang_zfd/LinTianyu/Internship/stable-diffusion/data/synapse",
-    #     test="/GPUFS/nsccgz_ywang_zfd/LinTianyu/Internship/stable-diffusion/data/synapse"
-    # )
-    # ../lists: .txt files
-    txt_path = dict(
-        train=os.path.join(root_path, 'lists', dataset, 'train.txt'),
-        test=os.path.join(root_path, 'lists', dataset, 'test.txt')
     )
 
     train_image_path = sorted(glob.glob(train_path['image'] + '/*.nii.gz'))
