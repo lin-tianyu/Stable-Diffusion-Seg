5e07080911e1057f91778a16e6307c11e45b3add
lin-tianyu <linty6@mail2.sysu.edu.cn>
2024-03-13 18:21:43 +0800

    update  ckpt sownload script

--- scripts/download_models_lsun_churches.sh
+++ scripts/download_models_lsun_churches.sh
@@ -2,7 +2,7 @@
 wget -O models/ldm/lsun_churches256/lsun_churches-256.zip https://ommer-lab.com/files/latent-diffusion/lsun_churches.zip
 
 
-cd ../lsun_churches256
+cd models/lsun_churches256
 unzip -o lsun_churches-256.zip
 
 
