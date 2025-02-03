fe69b0f9691085562930ff8f78f157908f49cd3b
Arinjoy <122046307+EchoCoder1729@users.noreply.github.com>
2025-02-04 01:30:53 +0530

    Add 'class_embed' to assertion values

--- ldm/models/diffusion/SDSeg.py
+++ ldm/models/diffusion/SDSeg.py
@@ -2180,7 +2180,7 @@ class DiffusionWrapper(pl.LightningModule):
         super().__init__()
         self.diffusion_model = instantiate_from_config(diff_model_config)
         self.conditioning_key = conditioning_key
-        assert self.conditioning_key in [None, 'concat', 'crossattn', 'hybrid', 'adm']
+        assert self.conditioning_key in [None, 'concat', 'crossattn', 'hybrid', 'adm', 'class_embed']
 
     def forward(self, x, t, c_concat: list = None, c_crossattn: list = None):
         if self.conditioning_key is None:
